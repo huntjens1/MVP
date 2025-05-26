7f347e8864a7245e0efd55b967190c26307ae741
huntjens1 <kevin_huntjens@live.nl>
2025-05-26 23:27:41 +0200

    update

--- frontend/src/Root.tsx
+++ frontend/src/Root.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthContext";
 import App from "./App";
 import ResetPassword from "./components/ResetPassword";
 
 export default function Root() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route path="/auth/confirm" element={<ResetPassword />} />
-        <Route path="/auth/callback" element={<ResetPassword />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/reset-password" element={<ResetPassword />} />
+          <Route path="/auth/confirm" element={<ResetPassword />} />
+          <Route path="/auth/callback" element={<ResetPassword />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
