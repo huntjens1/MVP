60c3f14e4cee4adae68ea853d90ac562b9dd89ec
huntjens1 <kevin_huntjens@live.nl>
2025-05-27 18:02:57 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import DashboardLayoutRouter from "./components/DashboardLayoutRouter";
 import DashboardHome from "./components/DashboardHome";
 import CallLogixTranscriptie from "./components/CallLogixTranscriptie";
@@ -9,12 +9,12 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/app" replace />} />
         <Route path="/app" element={<DashboardLayoutRouter />}>
           <Route index element={<DashboardHome />} />
           <Route path="transcriptie" element={<CallLogixTranscriptie />} />
           <Route path="geschiedenis" element={<OpnameGeschiedenis />} />
         </Route>
-        {/* Auth/wachtwoord reset buiten layout */}
         <Route path="/reset-password" element={<ResetPassword />} />
       </Routes>
     </BrowserRouter>
