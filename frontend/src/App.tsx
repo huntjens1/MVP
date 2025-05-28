2ac822eae028ce94730c83c0fc33b4b85bcb8d4d
huntjens1 <kevin_huntjens@live.nl>
2025-05-28 21:25:42 +0200

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,38 +1,34 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthPanel from "./components/AuthPanel";
 import DashboardLayoutRouter from "./components/DashboardLayoutRouter";
-import DashboardHome from "./components/DashboardHome";
+import ProtectedRoute from "./components/ProtectedRoute";
+import LandingPage from "./LandingPage";
+import AdminDashboard from "./components/AdminDashboard";
 import CallLogixTranscriptie from "./components/CallLogixTranscriptie";
 import OpnameGeschiedenis from "./components/OpnameGeschiedenis";
-import ResetPassword from "./components/ResetPassword";
-import AuthPanel from "./components/AuthPanel";
-import LandingPage from "./LandingPage";
-import ProtectedRoute from "./components/ProtectedRoute";
+import DashboardHome from "./components/DashboardHome";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Publieke routes */}
         <Route path="/" element={<LandingPage />} />
         <Route path="/auth" element={<AuthPanel />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
 
-        {/* Beveiligde dashboardroutes */}
         <Route
-          path="/app/*"
+          path="/app"
           element={
             <ProtectedRoute>
               <DashboardLayoutRouter />
             </ProtectedRoute>
           }
         >
+          {/* Subroutes voor /app/* */}
           <Route index element={<DashboardHome />} />
           <Route path="transcriptie" element={<CallLogixTranscriptie />} />
           <Route path="geschiedenis" element={<OpnameGeschiedenis />} />
+          <Route path="admin" element={<AdminDashboard />} />
         </Route>
-
-        {/* Fallback */}
-        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
