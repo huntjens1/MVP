5739a8c6eded051e0cef35c150cd692dd278662c
huntjens1 <kevin_huntjens@live.nl>
2025-05-28 20:39:04 +0200

    update

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,8 @@
+import { login } from '../controllers/authController.js';
 import express from 'express';
-import { register, login } from '../controllers/authController.js';
+
 const router = express.Router();
 
-router.post('/api/register', register);
 router.post('/api/login', login);
 
 export default router;
