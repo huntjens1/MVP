b3700068b34a23236e96e16f18bbc8743c5c6699
huntjens1 <kevin_huntjens@live.nl>
2025-05-28 16:44:30 +0200

    update

--- frontend/src/components/AuthPanel.tsx
+++ frontend/src/components/AuthPanel.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useAuth } from "./AuthContext";
+import { useAuth } from "../AuthContext";
 
 export default function AuthPanel() {
   const { isLoading, user, login, register } = useAuth();
